82e6d594fa2ca7d3afe83c5d98996263a4226826
vincent <vincent@clickinglabs.com>
2012-06-18 18:32:06 +0800

    Added topic model

--- app/models.rb
+++ app/models.rb
@@ -5,4 +5,26 @@ class User
   field :name, type: String
   field :access_token, type: String
 
+  def first_name
+    self.name.split.first
+  end
+
+end
+
+class Topic
+  include Mongoid::Document
+
+  field :title, type: String
+  
+  validates_uniqueness_of :title
+
+  def urlify
+    self.title.downcase.gsub(" ", "_")
+  end
+end
+
+class String
+  def titlefy
+    self.split("_").each { |word| word.capitalize! }.join(" ")
+  end
 end
