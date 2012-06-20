b17b601fa3450ee02d3d91aefc4127bf74f6a54f
vincent <vincent@clickinglabs.com>
2012-06-20 11:46:08 +0800

    Some indent fixes

--- app/routes.rb
+++ app/routes.rb
@@ -112,25 +112,25 @@ class Routes < Sinatra::Base
     end
  
     def distance_of_time_in_words(minutes)
-     case
-       when minutes < 1
-         "less than a minute"
-       when minutes < 50
-         "#{minutes} minutes"
-       when minutes < 90
-         "about one hour"
-       when minutes < 1080
-         "#{(minutes / 60).round} hours"
-       when minutes < 1440
-         "one day"
-       when minutes < 2880
-         "about one day"
-       else
-         "#{(minutes / 1440).round} days"
-     end
-   end
+      case
+        when minutes < 1
+          "less than a minute"
+        when minutes < 50
+          "#{minutes} minutes"
+        when minutes < 90
+          "about one hour"
+        when minutes < 1080
+          "#{(minutes / 60).round} hours"
+        when minutes < 1440
+          "one day"
+        when minutes < 2880
+          "about one day"
+        else
+          "#{(minutes / 1440).round} days"
+      end
+    end
   end
-
+  #end helpers ------
 end
 
 
