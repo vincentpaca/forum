c56b9cf1c438d24747976422c7781a6b030d0c71
Vincent Paca <vincent.paca@gmail.com>
2012-06-20 15:55:38 +0800

    Don't use twitter auth from now. Can't seem to test it locally

--- app/routes.rb
+++ app/routes.rb
@@ -18,11 +18,12 @@ class Routes < Sinatra::Base
   end
 
   get '/twitter/connect/?' do
-    session['oauth'] = OAuth::Consumer.new(TW_KEY, TW_SECRET, { :site => 'http://api.twitter.com' })
-    request_token = session['oauth'].get_request_token(:oauth_callback => base_url + '/twitter/callback')
-    session['token'] = request_token.token
-    session['secret'] = request_token.secret
-    redirect "http://api.twitter.com/oauth/authorize?oauth_token=#{session['token']}"
+    #session['oauth'] = OAuth::Consumer.new(TW_KEY, TW_SECRET, { :site => 'http://api.twitter.com' })
+    #request_token = session['oauth'].get_request_token(:oauth_callback => base_url + '/twitter/callback')
+    #session['token'] = request_token.token
+    #session['secret'] = request_token.secret
+    #redirect "http://api.twitter.com/oauth/authorize?oauth_token=#{session['token']}"
+    redirect '/'
   end
 
   get '/logout/?' do
@@ -36,15 +37,16 @@ class Routes < Sinatra::Base
     graph = Facebook::API.new(access_token)
     person = graph.get_object("me")
     session['current_user'] = User.find_or_create_by(:fb_id => person['id'], :name => person['name'], :access_token => access_token)
-	redirect '/'
+    redirect '/'
   end
   
   get '/twitter/callback/?' do
-    request = OAuth::RequestToken.new(session['oauth'], session['token'], session['secret'])
-    access = request.get_access_token(:oauth_verifier => params[:oauth_verifier])
-    response = session['oauth'].request(:get, '/account/verify_credentials.json', access, { :scheme => :query_string })
-    user_info = JSON.parse(response.body)
-    user_info
+    #request = OAuth::RequestToken.new(session['oauth'], session['token'], session['secret'])
+    #access = request.get_access_token(:oauth_verifier => params[:oauth_verifier])
+    #response = session['oauth'].request(:get, '/account/verify_credentials.json', access, { :scheme => :query_string })
+    #user_info = JSON.parse(response.body)
+    #user_info
+    redirect '/'
   end
   #end auth ------
 
